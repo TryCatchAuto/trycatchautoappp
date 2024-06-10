4602ca00b59c72dbcc6c453308eb01a8a446e48c
unknown <jm53957@zut.edu.pl>
2024-06-10 02:52:41 +0200

    Jeszcze jeden naprawiony blad po mergu. Chyba git juz powinien byc git

--- TryCatchAuto/src/model/Driver.java
+++ TryCatchAuto/src/model/Driver.java
@@ -99,14 +99,14 @@ public class Driver {
     /**
      * prints driver to standard output
      */
-    public void print(){
-        System.out.println("Driver ID: " + driver_id);
-        System.out.println("Last Name: " + lastName);
-        System.out.println("First Name: " + firstName);
-        System.out.println("Email: " + email);
-        System.out.println("Login: " + login);
-        System.out.println("Emp Status: " + employmentStatus);
-    }
+//    public void print(){
+//        System.out.println("Driver ID: " + driver_id);
+//        System.out.println("Last Name: " + lastName);
+//        System.out.println("First Name: " + firstName);
+//        System.out.println("Email: " + email);
+//        System.out.println("Login: " + login);
+//        System.out.println("Emp Status: " + employmentStatus);
+//    }
     public static String checkLogIn(DataBaseConnection conn,String login,String password){
         //String result=conn.CheckLoginDriver(login,password);
 
